798aacad463e7c6cbc3e6d783adffd340c1e53cc
laLaHuangGua <1270562182@qq.com>
2023-02-26 22:07:23 +0800

    DRQ Attempt1: bulid basic structure of Deque

---
+++ DRQ/codes/attempt1/Deque.java
@@ -0,0 +1,55 @@
+package DRQ.codes.attempt1;
+
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+  // construct an empty deque
+  public Deque() {
+
+  }
+
+  // is the deque empty?
+  public boolean isEmpty() {
+    return false;
+
+  }
+
+  // return the number of items on the deque
+  public int size() {
+    return 0;
+
+  }
+
+  // add the item to the front
+  public void addFirst(Item item) {
+
+  }
+
+  // add the item to the back
+  public void addLast(Item item) {
+
+  }
+
+  // remove and return the item from the front
+  public Item removeFirst() {
+    return null;
+
+  }
+
+  // remove and return the item from the back
+  public Item removeLast() {
+    return null;
+
+  }
+
+  @Override
+  public Iterator<Item> iterator() {
+
+    throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
