3ad485b0ef3bac8e5e390cc9a51707f60b7adf64
laLaHuangGua <1270562182@qq.com>
2023-02-28 10:56:42 +0800

    DRQ Attempt2: start from a copy of Attempt1

---
+++ DRQ/codes/attempt2/Permutation.java
@@ -0,0 +1,22 @@
+package DRQ.codes.attempt2;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class Permutation {
+  public static void main(String[] args) {
+    int count = Integer.parseInt(args[0]);
+    var queue = new RandomizedQueue<String>();
+
+    while (!StdIn.isEmpty()) {
+      String item = StdIn.readString();
+      queue.enqueue(item);
+    }
+
+    for (String each : queue) {
+      System.out.println(each);
+      count--;
+      if (count <= 0)
+        break;
+    }
+  }
+}
