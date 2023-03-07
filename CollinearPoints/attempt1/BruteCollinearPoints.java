c1b6fdd19ddfe8fcc6b02daef075cef4cc637ee3
laLaHuangGua <1270562182@qq.com>
2023-03-07 10:21:44 +0800

    CoP Attempt1: add Java file BruteCollinearPoints

---
+++ CollinearPoints/attempt1/BruteCollinearPoints.java
@@ -0,0 +1,20 @@
+package attempt1;
+
+public class BruteCollinearPoints {
+
+  // finds all line segments containing 4 points
+  public BruteCollinearPoints(Point[] points) {
+
+  }
+
+  // the number of line segments
+  public int numberOfSegments() {
+    return 0;
+
+  }
+
+  // the line segments
+  public LineSegment[] segments() {
+    return null;
+  }
+}
