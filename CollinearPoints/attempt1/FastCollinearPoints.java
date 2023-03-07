8f782bd1189ca0c0725cbd53af984092230b87c3
laLaHuangGua <1270562182@qq.com>
2023-03-07 19:43:34 +0800

    CoP Attempt1: add Java file FastCollinearPoints

---
+++ CollinearPoints/attempt1/FastCollinearPoints.java
@@ -0,0 +1,17 @@
+package attempt1;
+
+public class FastCollinearPoints {
+  LineSegment[] lineSegments;
+
+  public FastCollinearPoints(Point[] points) {
+
+  }
+
+  public int numberOfSegments() {
+    return 0;
+  }
+
+  public LineSegment[] segments() {
+    return lineSegments;
+  }
+}
